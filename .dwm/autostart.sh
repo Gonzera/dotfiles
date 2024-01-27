da55a8a8fc6829daba354dc50ee54c6aa406ac73
Gonzera <46120804+Gonzera@users.noreply.github.com>
2024-01-26 23:54:07 -0300

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -11,5 +11,8 @@ function run {
 run picom &
 run slstatus &
 run qbittorrent &
-run "feh --bg-fill $HOME/.wallpapers/lwa01.jpg --bg-scale $HOME/.wallpapers/lwa02.png" &
+run xbanish &
+run rescrobbled &
+run "feh --bg-fill $HOME/.wallpapers/misc01.jpg --bg-scale $HOME/.wallpapers/2bit.png --bg-scale $HOME/.wallpapers/2bit.png" &
+run "weeb_helper -d" &
 run "/opt/urserver/urserver --daemon"
