cb60a462764d2b08f8566ea05f692c47704bd6cd
Gonzera <46120804+Gonzera@users.noreply.github.com>
2026-02-03 08:57:05 -0300

    new year new neovim config

---
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "storm", -- or "night", "moon", "day"
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
